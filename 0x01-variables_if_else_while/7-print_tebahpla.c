0363c88949f76c690ca50f97d720a743992929c3
ugeh stanley <lildween1998@gmail.com>
2022-10-14 23:27:53 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,18 @@
-8-print_base16.c 9-print_comb.c
+#include <stdio.h>
+
+/**
+* main - prints is posetive, negative
+* or zero
+*Return: returns zero if program exits well.
+ */
+int main(void)
+{
+int c = 0;
+while (c <= 9)
+{
+putchar(48 + c);
+c++;
+}
+putchar('\n');
+return (0);
+}
