e161707fc66e5dc2a2e59fd631643c457841d5b2
ugeh stanley <lildween1998@gmail.com>
2022-10-14 23:27:23 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+/**
+* main - prints is posetive, negative
+* or zero
+*Return: returns zero if program exits well.
+ */
+int main(void)
+{
+int c = 0;
+while (c <= 9)
+{
+putchar(48 + c);
+c++;
+}
+putchar('\n');
+return (0);
+}
