eb010e34534c487214579ab11fbc0c2ede15ca9e
ugeh stanley <lildween1998@gmail.com>
2022-10-14 23:20:46 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+/**
+* main - prints is posetive, negative
+* or zero
+*Return: returns zero if program exits well.
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+if (c != 'e' && c != 'q')
+{
+putchar(c);
+}
+c++;
+}
+putchar('\n');
+return (0);
+}
