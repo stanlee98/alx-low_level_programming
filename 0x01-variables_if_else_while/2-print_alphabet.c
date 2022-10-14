92099c21f5b743a25feca86e6d0a54df6e9b4a51
ugeh stanley <lildween1998@gmail.com>
2022-10-14 23:09:41 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+/**
+* main - prints is posetive, negative
+* or zero
+*Return: returns zero if program exits well.
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
