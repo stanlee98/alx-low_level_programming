986632f5f7007f25b6af15f4108de5fbee121252
ugeh stanley <lildween1998@gmail.com>
2022-10-27 01:23:35 +0800

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,32 @@
+#include "main.h"
+/**
+ *_strncat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will be changed
+ *@n: value
+ *Return: dest
+ */
 
+char *_strncat(char *dest, char *src, int n)
+
+{
+int k, j;
+
+k = 0;
+while (dest[k] != '\0')
+{
+k++;
+}
+
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[k] = src[j];
+j++;
+k++;
+}
+
+dest[k] = '\0';
+
+return (dest);
+}
